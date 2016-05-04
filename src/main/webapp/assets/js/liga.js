3454bfe350d7400299a1c86d8bbcda842d6a4b2a
Aracki <aracki.ivan@gmail.com>
2016-05-04 16:31:22 +0200

    Added takmicari to liga page

--- src/main/webapp/assets/js/liga.js
+++ src/main/webapp/assets/js/liga.js
@@ -1,11 +1,17 @@
 var currentId;
+var takmicari;
 
 $(function(){
     
     currentId = location.search.split('=')[1];
     // load page
     ucitajLigu();    
+    ucitajTakmicare();
 });
+
+$('#btnBack').click(function(){
+    window.location.href = baseUrl + 'pocetna.html';
+})
 
 
 function ucitajLigu(){
@@ -26,7 +32,64 @@ function ucitajLigu(){
       });
 };
 
+function ucitajTakmicare() {
+    $.ajax({
+        url: 'http://localhost:8084/tenis/rest/takmicar?liga=' + currentId,
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': getCookie('token')
+        },
+        success: function (response) {
+            takmicari = response;
+            napuniTabeluTakmicara();
+        }
+
+    });
+}
+
 function napuniStranicu(ligaJson){
     console.log(ligaJson);
     $('#ligaNaslov').text(ligaJson.naziv);
+}
+
+function napuniTabeluTakmicara() {
+    if (typeof takmicari !== "undefined") {
+        var table = document.getElementById('tabelaTakmicari');
+        var table_body = document.createElement('TBODY');
+        table.border = '1';
+        table.appendChild(table_body);
+        var tHead = document.createElement('THEAD');
+        var arrayHeader = ["Ime", "Prezime", "Opis", "Broj pobeda"];
+        for (var i = 0; i < arrayHeader.length; i++) {
+            tHead.appendChild(document.createElement("TH")).appendChild(document.createTextNode(arrayHeader[i]));
+        }
+
+        for (var x = 0; x < takmicari.length; x++) {
+            var tr = document.createElement('TR');
+            table_body.appendChild(tr);
+            for (var j = 0; j < 4; j++) {
+                var td = document.createElement('TD');
+                td.width = '50';
+                switch (j) {
+                    case 0:
+                        td.id = "td_id";
+                        td.appendChild(document.createTextNode(takmicari[x].ime));
+                        break;
+                    case 1:
+                        td.appendChild(document.createTextNode(takmicari[x].prezime));
+                        break;
+                    case 2:
+                        td.appendChild(document.createTextNode(takmicari[x].opis));
+                        break;
+                    case 3:
+                        td.appendChild(document.createTextNode(takmicari[x].brojPobeda));
+                        break;
+                    default:
+                }
+                tr.appendChild(td);
+            }
+        }
+        table.appendChild(tHead);
+    }
 }
